1b61039152be7d37c8ef4927ab61885d178f360a
Brigham <brig.johnson@gmail.com>
2022-04-30 21:56:54 -0400

    commit tree 2494fe9c1bca7ac974b57986bbcd6c10d4a177ee

---
+++ index.js
@@ -0,0 +1,9 @@
+const add = (aa, bb) => aa + bb;
+
+const divide = (aa = 2000, bb = 100) => aa / bb;
+
+const square = (num) => num ** 2;
+
+console.log(divide(200, 10));
+console.log(add(2000, 100));
+console.log(square(100));
